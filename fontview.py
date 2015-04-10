a89fdaad3e378f678dd6919956a1cb19b35a6d49
Igor Viarheichyk <viarheichyk@google.com>
2015-04-10 12:55:31 -0700

    Ligatures and alternate glyphs support.

--- fontview.py
+++ fontview.py
@@ -10,21 +10,31 @@ Reports are generated in PDF format and contain font-specific rendering of
 the glyphs supported by the font.
 
 """
-from collections import namedtuple
 import os
-import sys
+import re
 import subprocess
+import sys
 import unicodedata
 
-from fontTools.ttLib import TTFont, TTLibError
+from fontTools.ttLib import TTFont
 
-class Glyph:
+
+class Glyph(object):
+  """Representation of a single glyph.
+
+  Contains glyph properties collected from different tables of a font and
+  queried by report classes.
+  """
+
   def __init__(self, name):
     self.name = name
     self.advanceWidth = None
     self.lsb = None
     self.classDef = 0
     self.caretList = None
+    self.sequences = None
+    self.alternates = None
+    self.index = -1
 
   def isLigature(self):
     return self.caretList or self.classDef == 2
@@ -47,10 +57,14 @@ class FontFile(object):
     self.ttf = TTFont(filename, fontNumber=-1, lazy=True)
     self._names = {}
     self.chars = {}
+    self._glyphsmap = {}
     self.glyphs = []
+    self.ligatures = {}
+    self.alternates = {}
     self._ParseNames()
-    self._ParseGlyphs()
     self._ParseCmap()
+    self._ParseGSUB()
+    self._ParseGlyphs()
 
   def _ParseNames(self):
     if 'name' in self.ttf:
@@ -75,7 +89,7 @@ class FontFile(object):
     author = self.GetName('Designer')
     manufacturer = self.GetName('Manufacturer')
     if author and manufacturer and author != manufacturer:
-      return "%s (%s)" % (author, manufacturer)
+      return '%s (%s)' % (author, manufacturer)
     elif author:
       return author
     else:
@@ -88,35 +102,61 @@ class FontFile(object):
           for code, name in table.cmap.items():
             self.chars[code] = name
 
+  def _ParseGSUB(self):
+    if 'GSUB' not in self.ttf:
+      return
+
+    for lookup in self.ttf['GSUB'].table.LookupList.Lookup:
+      for sub in lookup.SubTable:
+        if sub.LookupType == 3:
+          self.alternates.update(sub.alternates)
+        elif sub.LookupType == 4:
+          for key, value in sub.ligatures.iteritems():
+            for component in value:
+              sequence = tuple([key] + component.Component)
+              glyph = component.LigGlyph
+              if glyph not in self.ligatures:
+                self.ligatures[glyph] = set()
+              self.ligatures[glyph].add(sequence)
+
   def _ParseGlyphs(self):
     """Fetch available glyphs."""
     classDefs = {}
-    classNames = {2:'ligature', 3:'mark', 4: 'component' }
+    classNames = {2: 'ligature', 3: 'mark', 4: 'component'}
     caretList = {}
     metrics = {}
     if 'GDEF' in self.ttf:
       classDefs = self.ttf['GDEF'].table.GlyphClassDef.classDefs
       fontCaretList = self.ttf['GDEF'].table.LigCaretList
       if fontCaretList:
-        carets = [tuple(x.Coordinate for x in y.CaretValue) for y in fontCaretList.LigGlyph]
+        carets = [tuple(x.Coordinate for x in y.CaretValue)
+                  for y in fontCaretList.LigGlyph]
         caretList = dict(zip(fontCaretList.Coverage.glyphs, carets))
 
     if 'hmtx' in self.ttf:
       metrics = self.ttf['hmtx'].metrics
 
-    for name in self.ttf.getGlyphOrder():
+    for idx, name in enumerate(self.ttf.getGlyphOrder()):
       glyph = Glyph(name)
+      glyph.index = idx
       glyph.advanceWidth, glyph.lsb = metrics.get(name, [None, None])
       glyph.classDef = classDefs.get(name, 0)
       glyph.className = classNames.get(glyph.classDef, None)
       glyph.caretList = caretList.get(name, ())
+      glyph.sequences = self.ligatures.get(name, None)
+      glyph.alternates = self.alternates.get(name, None)
+      glyph.chars = [k for k, v in self.chars.iteritems() if v == name]
       self.glyphs.append(glyph)
+      self._glyphsmap[name] = glyph
 
   def GetName(self, name, default=None):
     return self._names.get(self.NAME_CODES[name], default)
 
   def GetNames(self):
-    return ['%d: %s' % (k, v) for k,v in sorted(self._names.iteritems())]
+    return ['%d: %s' % (k, v) for k, v in sorted(self._names.iteritems())]
+
+  def GetGlyph(self, name):
+    return self._glyphsmap[name]
 
 
 class Report(object):
@@ -135,10 +175,15 @@ class Report(object):
     pass
 
   def Xetex(self):
-    return self.TETEX_HEADER + self.XetexBody() + self.TETEX_FOOTER
+    body = self.XetexBody()
+    if body:
+      body = self.TETEX_HEADER + body + self.TETEX_FOOTER
+    return body
 
 
 class UnicodeCoverageReport(Report):
+  """Report font unicode coverage."""
+
   TETEX_HEADER = r'''
     \definecolor{missing}{gray}{.95}
     \begin{longtable}[l]{|r|l|p{0.6\textwidth}|}
@@ -158,7 +203,7 @@ class UnicodeCoverageReport(Report):
         uniname = unicodedata.name(unichr(code))
       except ValueError:
         uniname = ''
-      data += "U+%04d %s %s\n" % (code, name, uniname)
+      data += '  U+%04d %-30s %s\n' % (code, name, uniname)
     return data
 
   def XetexBody(self):
@@ -174,6 +219,7 @@ class UnicodeCoverageReport(Report):
       prevcode = code
       data += '\\texttt{%04X} & {\\customfont\\symbol{%d}} & {\\small %s}\\\\\n' % (code, code, uniname)
     return data
+
 
 class GlyphsReport(Report):
   NAME = 'Glyphs'
@@ -196,8 +242,10 @@ class GlyphsReport(Report):
 
   def Plaintext(self):
     data = ''
-    for idx, glyph in enumerate(self.font.glyphs):
-      data += "%6d %-30s %6d %6d %3d\n" % (idx, glyph.name, glyph.advanceWidth, glyph.lsb, glyph.classDef)
+    for glyph in self.font.glyphs:
+      data += '%6d %-30s %6d %6d %3d\n' % (
+          glyph.index, glyph.name, glyph.advanceWidth,
+          glyph.lsb, glyph.classDef)
     return data
 
   def XetexBody(self):
@@ -207,24 +255,26 @@ class GlyphsReport(Report):
       if name not in uni:
         uni[name] = []
       uni[name].append(code)
-    for idx, glyph in enumerate(self.font.glyphs):
+    for glyph in self.font.glyphs:
       if glyph.className:
         data += '\\rowcolor{%s}\n' % glyph.className
       if glyph.name in uni:
-        chars = ', '.join('u%04X' % x for x in uni[glyph.name])
+        chars = ', '.join('u%04X' % x for x in glyph.chars)
       else:
         chars = ''
-      data += "%d & {\customfont\XeTeXglyph %d} & %s & %d & %d & %d & %s\\\\\n" % (
-          idx, idx, TexEscape(glyph.name),
+      data += '%d & %s & %s & %d & %d & %d & %s\\\\\n' % (
+          glyph.index, TexGlyph(glyph), TexEscape(glyph.name),
           glyph.advanceWidth, glyph.lsb, glyph.classDef, chars)
     return data
 
+
 class LigaturesReport(Report):
+  """Report ligatures."""
   TETEX_HEADER = r'''
-    \begin{longtable}[l]{|l|l|l|}
+    \begin{longtable}[l]{|l|l|l|p{.5\textwidth}|}
     \hline
     \rowcolor{header}
-    Glyph & Name & Caret Positions \\
+    Glyph & Name & Caret Positions & Sequences \\
     \hline
     \endhead
     \hline
@@ -238,16 +288,69 @@ class LigaturesReport(Report):
     data = ''
     for glyph in self.font.glyphs:
       if glyph.isLigature():
-        data += "%-30s\t%s\n" % (glyph.name, glyph.caretList)
+        data += '%-30s\t%s\t%s\n' % (
+            glyph.name, ', '.join(glyph.caretList) if glyph.caretList else '-',
+            ', '.join(' '.join(x)
+                      for x in glyph.sequences) if glyph.sequences else '-')
     return data
 
   def XetexBody(self):
     data = ''
-    for idx, glyph in enumerate(self.font.glyphs):
+    for glyph in self.font.glyphs:
       if glyph.isLigature():
-        coords = ', '.join(str(x) for x in glyph.caretList) if glyph.caretList else ''
-        data += '{\customfont\XeTeXglyph %d} & %s & %s\\\\\n' % (idx, TexEscape(glyph.name), coords)
+        coords = ', '.join(
+            str(x) for x in glyph.caretList) if glyph.caretList else ''
+        items = []
+        if glyph.sequences:
+          for sequence in glyph.sequences:
+            seqitems = []
+            for name in sequence:
+              g = self.font.GetGlyph(name)
+              if g.chars:
+                path = ','.join('u%04X' % x for x in g.chars)
+              else:
+                path = name
+              seqitems.append('%s(%s)' % (TexGlyph(g), path))
+            items.append(' '.join(seqitems))
+        data += '%s & %s & %s & %s \\\\\n' % (
+            TexGlyph(glyph), TexEscape(glyph.name),
+            coords, ',\\newline '.join(items))
     return data
+
+
+class AlternatesReport(Report):
+  """Report alternate glyphs."""
+  TETEX_HEADER = r'''
+    \begin{longtable}[l]{|l|l|p{.7\textwidth}|}
+    \hline
+    \rowcolor{header}
+    Glyph & Name & Alternates \\
+    \hline
+    \endhead
+    \hline
+    \endfoot
+  '''
+  TETEX_FOOTER = r'\end{longtable}'
+
+  NAME = 'Alternates'
+
+  def Plaintext(self):
+    data = ''
+    for glyph in self.font.glyphs:
+      if glyph.alternates:
+        data += '%-30s\t%s\n' % (glyph.name, ', '.join(glyph.alternates))
+    return data
+
+  def XetexBody(self):
+    data = ''
+    for glyph in self.font.glyphs:
+      if glyph.alternates:
+        alternates = ', '.join('%s(%s)' % (
+            TexGlyph(self.font.GetGlyph(x)), x) for x in glyph.alternates)
+        data += '%s & %s & %s \\\\\n' % (
+            TexGlyph(glyph), TexEscape(glyph.name), alternates)
+    return data
+
 
 class SummaryReport(Report):
   TETEX_HEADER = r'''
@@ -305,35 +408,43 @@ class Envelope(Report):
     \newfontface\customfont[Path = %s/, Color = 0000AA]{%s}
   '''
 
-  KNOWN_REPORTS = (SummaryReport, UnicodeCoverageReport, GlyphsReport, LigaturesReport)
+  KNOWN_REPORTS = (SummaryReport, UnicodeCoverageReport,
+                   GlyphsReport, LigaturesReport, AlternatesReport)
 
   def Plaintext(self):
     data = ''
     for report in self.KNOWN_REPORTS:
       try:
-        data += report.NAME.upper() + '\n'
+        content = report(self.font).Plaintext()
+        if content:
+          data += report.NAME.upper() + '\n' + content + '\n\n'
       except AttributeError:
         pass
-      data += report(self.font).Plaintext() + '\n\n'
     return data
 
   def XetexBody(self):
     data = self.FONT_TEMPLATE % os.path.split(self.font.filename)
-    data += '\\title{%s}\n' % self.font.GetTitle()
-    data += '\\author{%s}\n' % self.font.GetAuthor()
-    data += '\\renewcommand\\today{%s}' % self.font.GetName('Version', 'Unknown version')
+    data += '\\title{%s}\n' % TexEscape(self.font.GetTitle())
+    data += '\\author{%s}\n' % TexEscape(self.font.GetAuthor())
+    data += '\\renewcommand\\today{%s}' % TexEscape(
+        self.font.GetName('Version', 'Unknown version'))
     data += '\\maketitle\n\\tableofcontents\n'
     for report in self.KNOWN_REPORTS:
       try:
-        data += '\\section{%s}\n' % report.NAME
+        content = report(self.font).Xetex()
+        if content:
+          data += '\\section{%s}\n%s\n' % (report.NAME, content)
       except AttributeError:
         pass
-      data += report(self.font).Xetex() + '\n'
     return data
 
 
+def TexGlyph(glyph):
+  return '{\\customfont\\XeTeXglyph %d}' % glyph.index
+
+
 def TexEscape(name):
-  return name.replace('_', r'\_').replace('#', r'\#')
+  return re.sub(r'([_#&{}\[\]])', r'\\\1', name)
 
 
 def main(argv):
@@ -344,7 +455,7 @@ def main(argv):
     infile = argv[1]
     outfile = None
   else:
-    print "Usage: %s infile [outfile]" % argv[0]
+    print 'Usage: %s infile [outfile]' % argv[0]
     sys.exit(-1)
 
   font = FontFile(infile)
