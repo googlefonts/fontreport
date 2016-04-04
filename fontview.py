545f50ae887d4c811d47f1a9da3705c4d17bc3ab
Igor Viarheichyk <viarheichyk@google.com>
2016-04-04 16:22:26 -0700

    Apache license file and header added.

--- fontview.py
+++ fontview.py
@@ -1,5 +1,20 @@
-# coding = utf-8
-"""Font internals reporting tool.
+#!/usr/bin/python
+#
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Font reporting tool.
 
 This command-line tool is able to generate various reports about the internals
 of the given TrueType or OpenType font, such as:
